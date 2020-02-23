f8bb0748cff2659b9941ad2b3f658042058f29c5
mkgame <mkgame2015@163.com>
2020-02-23 22:08:33 +0800

    Chapter10

---
+++ Example.py
@@ -0,0 +1,16 @@
+# def spam():
+#     banco()
+# def banco():
+#     raise Exception('This is a Exception')
+
+# spam() 
+
+# import traceback
+# try:
+#     raise Exception('This is a error message')
+# except:
+#     errorFile = open('errorInfo.txt','w')
+#     errorFile.write(traceback.format_exc())
+#     errorFile.close()
+#     print('The traceback info was written to errorInfo.txt')
+
